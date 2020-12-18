db9f58f6363bbfb329e7c8d73c5972adcebd7d2a
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-12-18 13:44:24 -0800

    worked on stay logged method

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,6 +17,7 @@ class HomeTableViewController: UITableViewController {
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
     }
     
     // MARK: - Table view data source
